4f8b3312c52d45f4a280c4883bbf8f74c6e207dc
Comum <miguel.rib.20@gmail.com>
2018-04-08 19:18:55 +0100

    Delete bookmark functionality

--- app/eggly-app.start.js
+++ app/eggly-app.start.js
@@ -85,6 +85,14 @@ angular.module('Eggly', [
     $scope.updateBookmark = updateBookmark;
     $scope.isSelectedBookmark = isSelectedBookmark;
 
+    function deleteBookmark(bookmark) {
+        _.remove($scope.bookmarks, function (b) {
+            return b.id === bookmark.id;
+        });
+    }
+
+    $scope.deleteBookmark = deleteBookmark;
+
     /** Create and editing */
 
     function startCreating() {
