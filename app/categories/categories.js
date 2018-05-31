af778f632c5d078793682c2baf75fd9024b95bd6
Comum <miguel.rib.20@gmail.com>
2018-05-31 18:26:11 +0100

    Using ui-router state params

--- app/categories/categories.js
+++ app/categories/categories.js
@@ -19,6 +19,4 @@ angular.module('categories', [
     })
     .controller('CategoriesCtrl', function CategoriesCtrl($scope) {
 
-    }).controller('BookmarksCtrl', function BookmarksCtrl($scope) {
-
     });
